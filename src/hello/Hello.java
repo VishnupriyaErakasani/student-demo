15fd729dd680658e010468454d570d294ca8bf7c
Erakasani Vishnu Priya <162144442+VishnupriyaErakasani@users.noreply.github.com>
2025-09-15 14:34:18 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,5 +2,8 @@ package hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Pipeline CI/CD!");
+        System.out.println("Jenkins pipeline Integration is done");
+
+        
     }
 }
