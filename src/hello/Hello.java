cc00b387fd5c1bc91e645c8fc3f4acf1f6ed17eb
Erakasani Vishnu Priya <162144442+VishnupriyaErakasani@users.noreply.github.com>
2025-09-20 10:40:05 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         System.out.println("Hello from Jenkins Pipeline CI/CD!");
         System.out.println("Jenkins pipeline Integration is done");
         System.out.println("Jenkins pipeline Integration Testing");
+        System.out.println("Jenkins Integration is done and adding webhook");
         
     }
 }
